7c59c6637775f4d20fb47c2f7a8f4aba33740ebf
Kittipat Poonyakariyakorn <kittipat_1413@hotmail.com>
2025-02-12 23:33:54 +0700

    docs: ConfigureDefaultMiddlewares function

--- framework/middleware/gin/default.go
+++ framework/middleware/gin/default.go
@@ -11,6 +11,28 @@ type DefaultMiddlewareConfig struct {
 	TracerProvider trace.TracerProvider
 }
 
+// ConfigureDefaultMiddlewares returns a slice of Gin handlers that constitute
+// a standardized "default" middleware chain for common features. These include:
+//
+//   - Recovery: Recovers from panics and returns a 500 Internal Server Error response.
+//   - CircuitBreaker: Monitors request failures and may trip to protect the service.
+//   - Trace: Instruments incoming requests with OpenTelemetry spans if a TracerProvider is given.
+//   - RequestID: Ensures each request has a unique identifier (injected in the header and context).
+//   - RequestLogger: Logs incoming requests with metadata such as method, route, and status code.
+//
+// The behavior of certain middlewares (e.g., tracing, logging) is influenced by
+// the fields in DefaultMiddlewareConfig. If the TracerProvider is nil, the global
+// trace provider is used. If the Logger is nil, a default logger is used.
+//
+// Example usage:
+//
+//	cfg := DefaultMiddlewareConfig{
+//	  Logger:         myLogger,
+//	  TracerProvider: myTracerProvider,
+//	}
+//
+//	router := gin.New()
+//	router.Use(ConfigureDefaultMiddlewares(cfg)...)
 func ConfigureDefaultMiddlewares(config DefaultMiddlewareConfig) []gin.HandlerFunc {
 	middlewares := []gin.HandlerFunc{
 		Recovery(),
