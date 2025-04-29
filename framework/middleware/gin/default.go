3fdd3aa1741cee2f39e90df6a9891f12db0f8238
Kittipat Poonyakariyakorn <kittipat_1413@hotmail.com>
2025-04-29 17:35:53 +0700

    fix: reorder middleware chain to ensure Recovery is called before CircuitBreaker

--- framework/middleware/gin/default.go
+++ framework/middleware/gin/default.go
@@ -35,11 +35,11 @@ type DefaultMiddlewareConfig struct {
 //	router.Use(ConfigureDefaultMiddlewares(cfg)...)
 func ConfigureDefaultMiddlewares(config DefaultMiddlewareConfig) []gin.HandlerFunc {
 	middlewares := []gin.HandlerFunc{
-		CircuitBreaker(),
+		Recovery(WithRecoveryLogger(config.Logger)),
 		Trace(WithTracerProvider(config.TracerProvider)),
 		RequestID(),
 		RequestLogger(WithRequestLogger(config.Logger)),
-		Recovery(),
+		CircuitBreaker(),
 	}
 	return middlewares
 }
